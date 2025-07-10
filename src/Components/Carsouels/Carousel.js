46d69faeafc83f41362467a8b60a85c34c450a37
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-07-11 00:52:07 +0530

    Test case -13

--- src/Components/Carsouels/Carousel.js
+++ src/Components/Carsouels/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useRef} from "react";
+import React, { useEffect, useRef, useState} from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
@@ -11,10 +11,14 @@ import RightNavButton from "./RightNavButton";
 const Carousel=({data, renderItem, breakpoints})=>{
     const prevRef=useRef(null);
     const nextRef=useRef(null);
-
+    const[swiperReady,setSwiperReady]=useState(false);
+        useEffect(()=>{
+            setSwiperReady(true);
+        },[]);
     return(
         <div className={styles.carouselContainer}>
             <LeftNavButton ref={prevRef} />
+            {swiperReady &&(
             <Swiper
                 modules={[Navigation]}
                 spaceBetween={20}
@@ -33,11 +37,11 @@ const Carousel=({data, renderItem, breakpoints})=>{
                 breakpoints={breakpoints}
                 className={styles.swiper}
             >
-                {Array.isArray(data) && 
-                    data.map((item)=>(
-                        <SwiperSlide key={item.id}>{renderItem(item)}</SwiperSlide>
-                    ))}
+                {data.map((item)=>(
+                    <SwiperSlide key={item.id}>{renderItem(item)}</SwiperSlide>
+                ))}
             </Swiper>
+            )}
         <RightNavButton ref={nextRef} />
     </div>
  )
