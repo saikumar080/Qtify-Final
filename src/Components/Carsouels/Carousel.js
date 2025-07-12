ca23fc6df17ea982c137d5de3dc9564a062e711d
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-07-12 12:31:44 +0530

    Test case -13 harder

--- src/Components/Carsouels/Carousel.js
+++ src/Components/Carsouels/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect,useRef} from "react";
+import React, { useRef} from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
@@ -11,13 +11,6 @@ import RightNavButton from "./RightNavButton";
 const Carousel=({data, renderItem, breakpoints})=>{
     const prevRef=useRef(null);
     const nextRef=useRef(null);
-    const[navigation,setNavigation]=useState(false);
-        useEffect(()=>{
-            setNavigation({
-                prevEl: prevRef.current,
-                nextEl: nextRef.current
-            });
-        },[]);
     return(
         <div className={styles.carouselContainer}>
             <div ref={prevRef}>
@@ -26,37 +19,40 @@ const Carousel=({data, renderItem, breakpoints})=>{
             <Swiper
                 modules={[Navigation]}
                 spaceBetween={20}
-                navigation={navigation}
-                loop={false}
-                onSwiper={(swiper)=>{
-                    setTimeout(()=>{
-                        swiper.params.navigation.prevEl=prevRef.current;
-                        swiper.params.navigation.nextEl=nextRef.current;
-                        swiper.navigation.destroy();
-                        swiper.navigation.init();
-                        swiper.navigation.update();
-                    })
+                navigation={{
+                    prevEl:prevRef.current,
+                    nextEl: nextRef.current
                 }}
+                onBeforeInit={(swiper)=>{
+                    swiper.params.navigation.prevEl=prevRef.current;
+                    swiper.params.navigation.nextEl=nextRef.current;
+                }}
+                slidesPerView={6}
+                slidesPerGroup={6}
                 breakpoints={{
-                0: {
-                slidesPerView: 1.2,
-                slidesPerGroup: 1,
-                spaceBetween: 12,
+                0:{
+                    slidesPerView:1.2,
+                    slidesPerGroup:1.2
+                },    
+                350: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                // spaceBetween: 12,
             },
             600: {
-                slidesPerView: 2.2,
-                slidesPerGroup: 2,
-                spaceBetween: 16,
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                //spaceBetween: 16,
             },
             960: {
-                slidesPerView: 3.2,
-                slidesPerGroup: 3,
-                spaceBetween: 20,
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+                //spaceBetween: 20,
             },
             1280: {
-                slidesPerView: 4.2,
-                slidesPerGroup: 4,
-                spaceBetween: 24,
+                slidesPerView: 6,
+                slidesPerGroup: 6,
+                //spaceBetween: 24,
             },
           }}
 
