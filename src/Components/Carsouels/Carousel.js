33d55486a425bb5595115962b4657ed8484d35a7
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-08-07 01:59:33 +0530

    RAM

--- src/Components/Carsouels/Carousel.js
+++ src/Components/Carsouels/Carousel.js
@@ -1,5 +1,5 @@
-import React, { useState,useEffect,useRef} from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
+import React, { useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -7,18 +7,22 @@ import styles from "./Carousel.module.css";
 import LeftNavButton from "./LeftNavButton";
 import RightNavButton from "./RightNavButton";
 
+const Carousel = ({ data, renderItem, breakpoints }) => {
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
+    const swiperRef = useRef(null);
 
-const Carousel=({data, renderItem, breakpoints})=>{
-    const prevRef=useRef(null);
-    const nextRef=useRef(null);
-    const[navigation,setNavigation]=useState(false);
-        useEffect(()=>{
-            setNavigation({
-                prevEl: prevRef.current,
-                nextEl: nextRef.current
-            });
-        },[]);
-    return(
+    useEffect(() => {
+        if (swiperRef.current && swiperRef.current.params) {
+            swiperRef.current.params.navigation.prevEl = prevRef.current;
+            swiperRef.current.params.navigation.nextEl = nextRef.current;
+            swiperRef.current.navigation.destroy(); // destroy previous bindings
+            swiperRef.current.navigation.init();    // init with new refs
+            swiperRef.current.navigation.update();  // update navigation state
+        }
+    }, [data]); // Re-run when data changes
+
+    return (
         <div className={styles.carouselContainer}>
             <div ref={prevRef} className={styles.LeftNavButton}>
                 <LeftNavButton />
@@ -26,58 +30,45 @@ const Carousel=({data, renderItem, breakpoints})=>{
             <Swiper
                 modules={[Navigation]}
                 spaceBetween={20}
-                navigation={navigation}
+                onSwiper={(swiper) => {
+                    swiperRef.current = swiper; // Save Swiper instance
+                }}
                 loop={false}
-                watchSlidesProgress={true}
-                //  virtualTranslate={true}
-onSwiper={(swiper) => {
-  console.log("swiper initialized with slidesPerView:", swiper.params.slidesPerView);
-  requestAnimationFrame(() => {
-    if (swiper.params.navigation && swiper.navigation && prevRef.current && nextRef.current) {
-      swiper.params.navigation.prevEl = prevRef.current;
-      swiper.params.navigation.nextEl = nextRef.current;
-      swiper.navigation.destroy();
-      swiper.navigation.init();
-      swiper.navigation.update();
-    }
-  });
-}}
-
                 breakpoints={breakpoints || {
-                0: {
-                slidesPerView: 2,
-                slidesPerGroup: 2,
-                spaceBetween: 12,
-            },
-            600: {
-                slidesPerView: 3,
-                slidesPerGroup: 3,
-                spaceBetween: 16,
-            },
-            960: {
-                slidesPerView: 4,
-                slidesPerGroup: 4,
-                spaceBetween: 20,
-            },
-            1280: {
-                slidesPerView: 6,
-                slidesPerGroup: 6,
-                spaceBetween: 24,
-            },
-          }}
-
-        className={styles.swiper}
+                    0: {
+                        slidesPerView: 2,
+                        slidesPerGroup: 2,
+                        spaceBetween: 12,
+                    },
+                    600: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 3,
+                        spaceBetween: 16,
+                    },
+                    960: {
+                        slidesPerView: 4,
+                        slidesPerGroup: 4,
+                        spaceBetween: 20,
+                    },
+                    1280: {
+                        slidesPerView: 6,
+                        slidesPerGroup: 6,
+                        spaceBetween: 24,
+                    },
+                }}
+                className={styles.swiper}
             >
-                {data.map((item)=>(
-                    <SwiperSlide key={item.id}>{renderItem(item)}</SwiperSlide>
+                {data.map((item) => (
+                    <SwiperSlide key={item.id}>
+                        {renderItem(item)}
+                    </SwiperSlide>
                 ))}
             </Swiper>
-          <div ref={nextRef} className={styles.RightNavButton}>
-                <RightNavButton/>
-          </div>  
-        
-    </div>
- )
+            <div ref={nextRef} className={styles.RightNavButton}>
+                <RightNavButton />
+            </div>
+        </div>
+    );
+};
 
-}
 export default Carousel;
